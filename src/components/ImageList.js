35afe004a1532fa56449cb688764465ab6b25312
GoranAviani <goranaviani@gmail.com>
2022-04-04 16:50:56 +0200

    react_using_refs_for_dom_access

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
-import './ImageList.css'
-
 import React from 'react';
 
+import './ImageList.css'
+import ImageCard from "./ImageCard";
+
 const ImageList = (props) => {
-    const images = props.images.map(({description, id, urls})=>{
+    const images = props.images.map((image)=>{
         return(
-                <img alt={description} key={id} src={urls.regular} />
+                <ImageCard key={image.id} image={image} />
         )
     });
 
