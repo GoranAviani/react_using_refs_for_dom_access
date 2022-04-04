c2a0c46467d6347e8d340e4f37fa2df110ef2792
GoranAviani <goranaviani@gmail.com>
2022-04-04 16:32:30 +0200

    react_using_refs_for_dom_access

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 
-class App extends React.Component{
+import unsplash from "../api/unsplash";
+import ImageList from "./ImageList";
+import SearchBar from "./SearchBar";
 
-    render(){
-        return(
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {images: []}
+    }
+
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos',
+            {
+                params: {
+                    query: term
+                }
+            });
+        this.setState({images: response.data.results})
+    }
+
+
+    render() {
+        return (
             <div>
-                App
+                <div className="ui container" style={{marginTop: '10px'}}>
+                    <SearchBar onSubmit={this.onSearchSubmit}/>
+                    <ImageList images={this.state.images}/>
+                </div>
             </div>
         )
-
     }
 }
 
